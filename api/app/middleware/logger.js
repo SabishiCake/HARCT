4b3c281936a83e5ff09bb113a9921fc23387a67d
Sabishi <pedjovensindol.edu@gmail.com>
2023-10-27 11:05:25 +0800

    - Updated to logger to include if access was granted

--- api/app/middleware/logger.js
+++ api/app/middleware/logger.js
@@ -1,16 +1,14 @@
-
 const fs = require('fs');
 const logName = `${new Date().toISOString().slice(0, 10)}.log`;
-const logFile = __dirname + '/../logs/' + logName ;
+const logFile = __dirname + '/../logs/' + logName;
 
 const createFile = () => {
-  fs.writeFile( logFile, '', (err) => {
+  fs.writeFile(logFile, '', (err) => {
     if (err) {
       console.error('Error creating log file:', err);
     }
   });
 };
-
 
 const logRequest = (req, res, next) => {
   const apiKey = req.headers['x-api-key'];
@@ -18,14 +16,20 @@ const logRequest = (req, res, next) => {
   if (!fs.existsSync(logFile)) {
     createFile();
   }
-  
-  const logMessage = `${new Date().toISOString()} - API Key: ${apiKey} - URL: ${req.originalUrl} - Method: ${req.method}\n`;
-  fs.appendFile(logFile, logMessage, (err) => {
-    if (err) {
-      console.error('Error writing to log file:', err);
-    }
+
+  const logMessage = `${new Date().toISOString()} - API Key: ${apiKey} - URL: ${req.originalUrl} - Method: ${req.method}`;
+  res.on('finish', () => {
+    const statusCode = res.statusCode;
+    const granted = (statusCode >= 200 && statusCode < 300) ? 'Granted' : 'Denied';
+    const logWithStatus = `${logMessage} - Status: ${statusCode} - Access: ${granted}\n`;
+    fs.appendFile(logFile, logWithStatus, (err) => {
+      if (err) {
+        console.error('Error writing to log file:', err);
+      }
+    });
   });
   next();
 };
+
 
 module.exports = logRequest;
