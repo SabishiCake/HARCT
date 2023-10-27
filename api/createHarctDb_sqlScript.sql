83fd545c9d0910d5bda63cefb37012b61f728eab
Sabishi <pedjovensindol.edu@gmail.com>
2023-10-27 13:07:19 +0800

    - update sql schema

--- api/createHarctDb_sqlScript.sql
+++ api/createHarctDb_sqlScript.sql
@@ -1,84 +1,208 @@
-CREATE DATABASE harctmydb;
-USE harctmydb;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: harctmydb
+-- ------------------------------------------------------
+-- Server version	8.0.34
 
-CREATE TABLE Guest (
-    GuestID INT PRIMARY KEY,
-    LastName VARCHAR(255),
-    FirstName VARCHAR(255),
-    Email VARCHAR(255),
-    Phone VARCHAR(255),
-    Address VARCHAR(255)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE Room (
-    RoomID INT PRIMARY KEY,
-    RoomNumber VARCHAR(255),
-    Type VARCHAR(255),
-    Description VARCHAR(255),
-    Price DECIMAL(10, 2)
-);
+--
+-- Table structure for table `apikeys`
+--
 
-CREATE TABLE Reservation (
-    ReservationID INT PRIMARY KEY,
-    GuestID INT,
-    RoomID INT,
-    CheckInDate DATE,
-    CheckOutDate DATE,
-    TotalCost DECIMAL(10, 2),
-    FOREIGN KEY (GuestID) REFERENCES Guest(GuestID),
-    FOREIGN KEY (RoomID) REFERENCES Room(RoomID)
-);
+DROP TABLE IF EXISTS `apikeys`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `apikeys` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `APIKey` varchar(255) NOT NULL,
+  `Description` varchar(255) DEFAULT NULL,
+  `Type` varchar(45) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE Staff (
-    StaffID INT PRIMARY KEY,
-    Name VARCHAR(255),
-    Position VARCHAR(255),
-    Email VARCHAR(255),
-    Phone VARCHAR(255)
-);
+--
+-- Table structure for table `feedback`
+--
 
-CREATE TABLE Orders (
-    OrderID INT PRIMARY KEY,
-    GuestID INT,
-    OrderDate DATE,
-    TotalPrice DECIMAL(10, 2),
-    FOREIGN KEY (GuestID) REFERENCES Guest(GuestID)
-);
+DROP TABLE IF EXISTS `feedback`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `feedback` (
+  `FeedbackID` int NOT NULL,
+  `GuestID` int DEFAULT NULL,
+  `Comment` varchar(255) DEFAULT NULL,
+  `Rating` int DEFAULT NULL,
+  PRIMARY KEY (`FeedbackID`),
+  KEY `GuestID` (`GuestID`),
+  CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`GuestID`) REFERENCES `guest` (`GuestID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE MenuItems (
-    ItemID INT PRIMARY KEY,
-    Name VARCHAR(255),
-    Description VARCHAR(255),
-    Price DECIMAL(10, 2)
-);
+--
+-- Table structure for table `guest`
+--
 
-CREATE TABLE OrderItems (
-    OrderItemID INT PRIMARY KEY,
-    OrderID INT,
-    ItemID INT,
-    Quantity INT,
-    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
-    FOREIGN KEY (ItemID) REFERENCES MenuItems(ItemID)
-);
+DROP TABLE IF EXISTS `guest`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `guest` (
+  `GuestID` int NOT NULL,
+  `LastName` varchar(255) DEFAULT NULL,
+  `FirstName` varchar(255) DEFAULT NULL,
+  `Email` varchar(255) DEFAULT NULL,
+  `Phone` varchar(255) DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`GuestID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE Payments (
-    PaymentID INT PRIMARY KEY,
-    GuestID INT,
-    Amount DECIMAL(10, 2),
-    PaymentDate DATE,
-    FOREIGN KEY (GuestID) REFERENCES Guest(GuestID)
-);
+--
+-- Table structure for table `menuitems`
+--
 
-CREATE TABLE Feedback (
-    FeedbackID INT PRIMARY KEY,
-    GuestID INT,
-    Comment VARCHAR(255),
-    Rating INT,
-    FOREIGN KEY (GuestID) REFERENCES Guest(GuestID)
-);
+DROP TABLE IF EXISTS `menuitems`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `menuitems` (
+  `ItemID` int NOT NULL,
+  `Name` varchar(255) DEFAULT NULL,
+  `Description` varchar(255) DEFAULT NULL,
+  `Price` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`ItemID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE APIKeys (
-    ID INT PRIMARY KEY AUTO_INCREMENT,
-    APIKey VARCHAR(255),
-    Description VARCHAR(255)
-);
+--
+-- Table structure for table `orderitems`
+--
+
+DROP TABLE IF EXISTS `orderitems`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orderitems` (
+  `OrderItemID` int NOT NULL,
+  `OrderID` int DEFAULT NULL,
+  `ItemID` int DEFAULT NULL,
+  `Quantity` int DEFAULT NULL,
+  PRIMARY KEY (`OrderItemID`),
+  KEY `OrderID` (`OrderID`),
+  KEY `ItemID` (`ItemID`),
+  CONSTRAINT `orderitems_ibfk_1` FOREIGN KEY (`OrderID`) REFERENCES `orders` (`OrderID`),
+  CONSTRAINT `orderitems_ibfk_2` FOREIGN KEY (`ItemID`) REFERENCES `menuitems` (`ItemID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `OrderID` int NOT NULL,
+  `GuestID` int DEFAULT NULL,
+  `OrderDate` date DEFAULT NULL,
+  `TotalPrice` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`OrderID`),
+  KEY `GuestID` (`GuestID`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`GuestID`) REFERENCES `guest` (`GuestID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `payments`
+--
+
+DROP TABLE IF EXISTS `payments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `payments` (
+  `PaymentID` int NOT NULL,
+  `GuestID` int DEFAULT NULL,
+  `Amount` decimal(10,2) DEFAULT NULL,
+  `PaymentDate` date DEFAULT NULL,
+  PRIMARY KEY (`PaymentID`),
+  KEY `GuestID` (`GuestID`),
+  CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`GuestID`) REFERENCES `guest` (`GuestID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `reservation`
+--
+
+DROP TABLE IF EXISTS `reservation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservation` (
+  `ReservationID` int NOT NULL,
+  `GuestID` int DEFAULT NULL,
+  `RoomID` int DEFAULT NULL,
+  `CheckInDate` date DEFAULT NULL,
+  `CheckOutDate` date DEFAULT NULL,
+  `TotalCost` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`ReservationID`),
+  KEY `GuestID` (`GuestID`),
+  KEY `RoomID` (`RoomID`),
+  CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`GuestID`) REFERENCES `guest` (`GuestID`),
+  CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`RoomID`) REFERENCES `room` (`RoomID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `room`
+--
+
+DROP TABLE IF EXISTS `room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `room` (
+  `RoomID` int NOT NULL,
+  `RoomNumber` varchar(255) DEFAULT NULL,
+  `Type` varchar(255) DEFAULT NULL,
+  `Description` varchar(255) DEFAULT NULL,
+  `Price` decimal(10,2) DEFAULT NULL,
+  `IsOccupied` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`RoomID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `staff`
+--
+
+DROP TABLE IF EXISTS `staff`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `staff` (
+  `StaffID` int NOT NULL,
+  `Name` varchar(255) DEFAULT NULL,
+  `Position` varchar(255) DEFAULT NULL,
+  `Email` varchar(255) DEFAULT NULL,
+  `Phone` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`StaffID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-27 13:04:11
